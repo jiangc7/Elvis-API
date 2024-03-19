20e0a6ecfa5c8342e8db2d22fcc8b989acf2fd88
Elvis Jiang <118397495+jiangc7@users.noreply.github.com>
2024-03-19 13:12:16 +1300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "root" #PUT YOUR MySQL username here - usually root
-dbpass = "dir123" #PUT YOUR PASSWORD HERE
+dbpass = "" #PUT YOUR PASSWORD HERE
 dbhost = "localhost" 
 dbport = "3306"
 dbname = "Elvisworks"
